8a016b592585f8a8bf421544ff1cb2a8eb09cc5d
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-11 09:55:46 +0530

    04 - 005 Styling for Android & iOS --shadow--

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton> Reset</PrimaryButton>
       <PrimaryButton> Confirm</PrimaryButton>
@@ -13,3 +13,20 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    // shadow for Android
+    elevation: 4,
+    // shadow for IOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
