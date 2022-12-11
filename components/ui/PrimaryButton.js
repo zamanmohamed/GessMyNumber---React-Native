8b62e18233eb68b33878afaad092fa78017e6e16
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-11 23:15:55 +0530

    04 - 020 Creating, Using & Displaying Random Numbers

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
-import Colors from "../constants/colors";
+import Colors from "../../constants/colors";
 
 const PrimaryButton = ({ children, onPress }) => {
   function pressHandler() {
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import Title from "../components/ui/Title";
 
-function GameScreen() {
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      {/* GUESS */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
         {/* + - */}
